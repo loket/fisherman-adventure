49c26a81712902be6549040915db0e3007027bae
loket <loket@cruftlab.io>
2017-10-20 12:54:15 +0200

    use components

--- src/Game.js
+++ src/Game.js
@@ -2,10 +2,30 @@
 import React from 'react';
 
 
-const Game = function () {
-  return (
-    <div>Hello react</div>
-  );
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: 'Hello',
+    }
+  }
+
+  onChangeInput = (event) => {
+    console.log("Changing value to " + event.target.value);
+    this.setState({
+      term: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={this.onChangeInput} />
+      </div>
+    );
+  }
 }
 
 export default Game;
