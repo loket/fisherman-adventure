e6285acff4d216ef0e5a2126c123728d4246fa3d
loket <loket@cruftlab.io>
2017-10-20 13:11:37 +0200

    added some checkers

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,13 @@
 import React from 'react';
 
 
+const pattern = [
+  [1,0,1],
+  [0,1,0],
+  [1,0,1],
+];
+const SIZE = 30, SPACE = 10;
+
 class Game extends React.Component {
   constructor(props) {
     super(props);
@@ -17,12 +24,34 @@ class Game extends React.Component {
     });
   }
 
+  renderTile = (obj, index, idx) => {
+    return (
+      <div
+        className="cell"
+        style={{
+          top: index * (SIZE + SPACE) + 'px',
+          left: idx * (SIZE + SPACE) + 'px',
+        }}
+      />
+    );
+  }
+
+  renderChecker = (obj) => (
+    obj.map((rows, index) =>
+    rows.map((column, idx) =>
+    this.renderTile(obj, index, idx)
+  )));
+
   render() {
     return (
       <div>
         <input
           value={this.state.term}
-          onChange={this.onChangeInput} />
+          onChange={this.onChangeInput}
+        />
+        <div className="checkersContainer">
+          {this.renderChecker(pattern)}
+        </div>
       </div>
     );
   }
